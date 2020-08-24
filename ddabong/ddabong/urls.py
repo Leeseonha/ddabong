7afe52c8fdb4eeb8b4b31794a840bd556f9dc8dc
Leeseonha <jmtkdsh@gmail.com>
2020-08-24 15:23:50 +0900

    html_connect

---
+++ ddabong/ddabong/urls.py
@@ -0,0 +1,29 @@
+"""ddabong URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+import contact.views
+import items.views
+import mypage.views
+import theme.views
+import voulunteer_work.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('theme.urls')),
+    path('v_area/',  voulunteer_work.views.v_area, name='v_area'),
+    path('mypage/',  mypage.views.mypage, name='mypage'),
+]
---
+++ ddabong/voulunteer_work/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+def v_area(request):
+    return render(request, 'v_area.html')
