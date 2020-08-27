46f4ba5d3a2b5ce90eba34bd096c1566d7cb85e0
JeongEun <lisasje25@gmail.com>
2020-08-27 17:47:58 +0900

    db down

--- ddabong/ddabong/settings.py
+++ ddabong/ddabong/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'ddabong.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ddabong_db',
+        'USER' : 'jesh',
+        'PASSWORD':'2728',
+        'HOST': 'localhost',
+        'PORT' : '3306',
+        'OPTIONS' : {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
+
     }
 }
 
